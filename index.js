14de3009de549eed98d62f2a0eae11c07f65474b
Daphna-che <osineyepearl@gmail.com>
2024-01-23 14:06:32 +0100

    pantherastoreapi

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 const express = require("express");
 const server = express();
+
+server.get("/", function (req, res) {
+  return res.status(200).json({
+    message: "Panthera store api service",
+  });
+});
+
+server.listen(4000, function () {
+  console.log("Service listening on port 4000");
+});
